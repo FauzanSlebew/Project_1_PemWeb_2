56d9fcc4a246be6d07d4d0b2b4647acd3e342180
FauzanSlebew <bangupit27@gmail.com>
2024-05-10 01:55:48 +0700

    Upload Github Project 1 PemWeb 2 Semester 2

---
+++ indeks.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project 1</title>
+</head>
+<link rel="icon" href="icon.png">
+<link rel="stylesheet" href="indeks.css">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+<script src="indeks.js"></script>
+
+<body>
+	
+<header>
+
+	<div class="overlay">
+	 <h1>Puskesmas Nambo</h1>
+     <h3>Mari Hidup Sehat</h3>
+     <p>Lebih Baik Menjaga Daripada Mengobati</p>
+	<a href="login.php"><button>Login</button></a>
+		</div>
+</header>
+
+
+
+
+<footer class="footer">
+  	 <div class="container">
+  	 	<div class="row">
+  	 		<div class="footer-col">
+  	 			<h4>Resources</h4>
+  	 			<ul>
+  	 				<li><a href="#">about us</a></li>
+  	 				<li><a href="#">our services</a></li>
+  	 				<li><a href="#">privacy policy</a></li>
+  	 				<li><a href="#">affiliate program</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>get help</h4>
+  	 			<ul>
+  	 				<li><a href="#">FAQ</a></li>
+  	 				<li><a href="#">shipping</a></li>
+  	 				<li><a href="#">returns</a></li>
+  	 				<li><a href="#">order status</a></li>
+  	 				<li><a href="#">payment options</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>follow us</h4>
+  	 			<div class="social-links">
+  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i class="fab fa-twitter"></i></a>
+  	 				<a href="#"><i class="fab fa-instagram"></i></a>
+  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
+  	 			</div>
+  	 		</div>
+  	 	</div>
+  	 </div>
+</footer>
+
+</body>
+</html>
+
+  </footer>
+  
+</body>
+</html>
+
+
+
